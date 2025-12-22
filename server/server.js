2165d8aa2f8ec7578f802e78239fc8ddfe06b541
Naveen32716e <naveenkumarreddykarri1@gmail.com>
2025-12-22 13:43:02 +0530

    serverjs modified

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: "10mb" }));
 
 app.use("/api/ai", aiRoutes);
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
